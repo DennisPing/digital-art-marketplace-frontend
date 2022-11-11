5485f381316ae45657de28507e4101de9b271aeb
qinxhannah <qin.xia@northeastern.edu>
2022-11-11 15:14:34 -0800

    hannah test

--- src/home/index.js
+++ src/home/index.js
@@ -4,6 +4,7 @@ const HomeComponent = () => {
       <h1>This is the Home page</h1>
       <h2>This is Dennis's test</h2>
       <h1>Sarah's test</h1>
+      <h1>Hannah's test</h1>
     </div>
   );
 };
