823ef726c75b3e0cc641653b92a7985f4db82c15
Dennis Ping <idennisping@gmail.com>
2022-11-11 17:07:05 -0500

    Basic page

---
+++ src/home/index.js
@@ -0,0 +1,9 @@
+const HomeComponent = () => {
+  return (
+    <div>
+      <h1>This is the Home page</h1>
+    </div>
+  );
+};
+
+export default HomeComponent;
---
+++ src/shop/index.js
@@ -0,0 +1,9 @@
+const ShopComponent = () => {
+  return (
+    <div>
+      <h1>This is the Shop page</h1>
+    </div>
+  );
+};
+
+export default ShopComponent;
