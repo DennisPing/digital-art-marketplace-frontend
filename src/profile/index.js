7372648d90f6e4cf0ac8fc76be1fca6afd1f6555
sbj <sbahnj@gmail.com>
2022-11-15 23:08:49 -0800

    Account info fields

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,8 +1,30 @@
 const ProfileComponent = () => {
   return (
     <div>
-      <h1>This is the Profile page</h1>
+      <h1>My Profile</h1>
+
+        <h4>Account Information</h4>
+
+        <h5>Address</h5>
+        User's current address appears here
+        <button type="button">Edit</button>
+      <h5>Email</h5>
+        User's current email appears here
+        <button type="button">Edit</button>
+        <h5>Phone</h5>
+        User's current phone appears here
+        <button type="button">Edit</button>
+
+        <button type="button">Delete Account</button>
+
+
     </div>
+
+
+
+
+
+
   );
 };
 
