2f65915b2e72b3290eed260205c775f5714d5ba5
Dennis Ping <idennisping@gmail.com>
2022-11-22 09:55:57 -0500

    Fixed product component bug, reverted theme from Zephyr back to default bootstrap

--- src/product/index.js
+++ src/product/index.js
@@ -1,30 +1,31 @@
 // product summary
 
-import product from "./product.json";
+import productArray from "./product.json";
 import ProductImage from "./sections/product-image";
-import ProductInfo from './sections/product-info';
-import { Row, Col } from 'antd';
+import ProductInfo from "./sections/product-info";
+import { Row, Col } from "antd";
 
 const ProductComponent = () => {
   return (
-      <div className="container" style={{ width: '100%', padding: '3rem 4rem' }}>
+    <div className="container" style={{ width: "100%", padding: "3rem 4rem" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h2>Smiling Ramen</h2>
+      </div>
+      <br />
 
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <h2>Smiling Ramen</h2>
-          </div><br />
-
-        <Row gutter={[56, 40]} >
-          <Col lg={12} xs={24}>
-            {/*<ProductImage product={product} />*/}
-            <img
-                src="https://i.seadn.io/gae/j6RkDDwMIu5LyxWE7Upuc401Dy7oNIZ-U1_wsCmRH13Nu5y9KOhpuNQbxWj8ZPNpeYqS6A0xyYp-WQToiM03z5-f21GjvgNy1Q4OhUw?auto=format&w=512"
-                className ="img-fluid"
-                alt="product_image"/>
-          </Col>
-          <Col lg={12} xs={24}>
-            <ProductInfo product={product} />
-          </Col>
-        </Row>
+      <Row gutter={[56, 40]}>
+        <Col lg={12} xs={24}>
+          {/*<ProductImage product={product} />*/}
+          <img
+            src="https://i.seadn.io/gae/j6RkDDwMIu5LyxWE7Upuc401Dy7oNIZ-U1_wsCmRH13Nu5y9KOhpuNQbxWj8ZPNpeYqS6A0xyYp-WQToiM03z5-f21GjvgNy1Q4OhUw?auto=format&w=512"
+            className="img-fluid"
+            alt="product_image"
+          />
+        </Col>
+        <Col lg={12} xs={24}>
+          <ProductInfo key={productArray[0].id} product={productArray[0]} />
+        </Col>
+      </Row>
     </div>
   );
 };
--- src/product/sections/product-info.js
+++ src/product/sections/product-info.js
@@ -1,30 +1,31 @@
-function ProductInfo({product}) {
+function ProductInfo({ product }) {
   return (
-      <div>
+    <div>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <h6> Description </h6>
+        </li>
+        <li className="list-group-item">
+          <h6> Price </h6>
+          <h6 className="text-secondary">{product.price}</h6>
+        </li>
+        <li className="list-group-item">
+          <h6> Creator </h6>
+          <h6 className="text-secondary">{product.creator}</h6>
+        </li>
+        <li className="list-group-item">
+          <h6> Views </h6>
+        </li>
+      </ul>
 
-        <ul className="list-group">
-          <li className="list-group-item">
-            <h6> Description </h6>
-          </li>
-          <li className="list-group-item">
-            <h6> Price </h6>
-          </li>
-          <li className="list-group-item">
-            <h6> Creator </h6>
-          </li>
-          <li className="list-group-item">
-            <h6> Views </h6>
-          </li>
-        </ul>
-
-        <br />
-        <br />
-        <br />
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <button className="btn btn-primary rounded-pill float-end">Add to Cart</button>
-        </div>
+      <br />
+      <br />
+      <br />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button className="btn btn-primary rounded-pill float-end">Add to Cart</button>
       </div>
-  )
+    </div>
+  );
 }
 
-export default ProductInfo
+export default ProductInfo;
