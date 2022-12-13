8e2153ee432c89a143ac0eef3d6d97654d4e876c
Dennis Ping <idennisping@gmail.com>
2022-12-13 17:42:06 -0500

    Fixed API_BASE syntax

--- src/services/product/product-service.js
+++ src/services/product/product-service.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE;
 
 export const getProduct = async (contractAddress, tokenId) => {
-  const response = await axios.get(`${API_BASE}/api/product/${contractAddress}/${tokenId}`);
+  const response = await axios.get(`${API_BASE}/product/${contractAddress}/${tokenId}`);
   return response.data;
 };
 
 export const getProductTransaction = async (contractAddress, tokenId) => {
-  const response = await axios.get(`${API_BASE}/api/product/tx/${contractAddress}/${tokenId}`);
+  const response = await axios.get(`${API_BASE}/product/tx/${contractAddress}/${tokenId}`);
   return response.data;
 };
