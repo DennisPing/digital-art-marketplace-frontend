0b87d3458cd70fbddaf33f73f8b4cd0c232a1892
sbj <sbahnj@gmail.com>
2022-12-13 11:02:50 -0800

    register user aka create user

--- src/register/index.js
+++ src/register/index.js
@@ -1,13 +1,17 @@
 import {Link} from "react-router-dom";
 import "./index.css"
-import {register} from "../services/userService";
+import {create} from "../services/userService";
 
 
 
 const user = {
 
     username: "",
-    password: ""
+    password: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: ""
 
 }
 
@@ -19,14 +23,22 @@ function registerHandler(){
 
     const username = document.getElementById("usernameField").value
     const password = document.getElementById("passwordField").value
+    const firstName = document.getElementById("firstNameField").value
+    const lastName = document.getElementById("lastNameField").value
+    const email = document.getElementById("emailField").value
+    const phone = document.getElementById("phoneField").value
     user.username = username
     user.password = password
+    user.firstName = firstName
+    user.lastName = lastName
+    user.email = email
+    user.phone = phone
 
     console.log(user)
 
     console.log("registerUser() reached")
 
-    register(user)
+    create(user)
 
 
 }
@@ -51,6 +63,30 @@ const RegisterComponent = () => {
 
                 className={"form-control"}
                 placeholder={"password"}
+            />
+
+            <input id={"firstNameField"}
+
+                   className={"form-control"}
+                   placeholder={"first name"}
+            />
+
+            <input id={"lastNameField"}
+
+                   className={"form-control"}
+                   placeholder={"last name"}
+            />
+
+            <input id={"emailField"}
+
+                   className={"form-control"}
+                   placeholder={"email"}
+            />
+
+            <input id={"phoneField"}
+
+                   className={"form-control"}
+                   placeholder={"phone"}
             />
 
             <button className={"register-button"} onClick={registerHandler} >Register</button>
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE;
 const USERS_API = `${API_BASE}/users`;
 
-export const register = async (user) => {
+// create/register
+export const create = async (user) => {
     const response = await axios
         .post(`${USERS_API}`, user);
     return response.data;
