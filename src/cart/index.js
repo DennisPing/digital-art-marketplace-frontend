50431abf986405edf3ba0f358e31f1a9fd9782b0
qinxhannah <qin.xia@northeastern.edu>
2022-12-13 19:36:50 -1000

    added stayle changes for cart items and checkout button

--- src/cart/index.js
+++ src/cart/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import cartItemArray from "./cartItems.json";
-import Total from './total';
-import CartItem from './cartItem';
+import Total from './sections/totalPrice/total';
+import CartItem from './sections/cartItem/cartItem';
 // import { useSelector } from 'react-redux';
 import {Link} from "react-router-dom";
 
@@ -13,7 +13,7 @@ function CartComponent() {
       <div className="cart">
         <div className="cart__left">
           <div>
-            <h3>Shopping Cart</h3>
+            <h2>Shopping Cart</h2>
             {cartItemArray.map((item) => (
                 <CartItem
                     key={item.id}
@@ -28,7 +28,7 @@ function CartComponent() {
 
         <div className="cart__right">
           <Total/>
-          <Link to="/checkout"><button className={'checkout-button'} >Checkout</button>
+          <Link to="/checkout"><button className="checkout-button text-white" >Checkout</button>
           </Link>
         </div>
       </div>
--- src/cart/sections/totalPrice/total.js
+++ src/cart/sections/totalPrice/total.js
@@ -1,5 +1,5 @@
 import './total.css';
-import cartItemArray from "./cartItems.json";
+import cartItemArray from "../../cartItems.json";
 // import {useSelector} from 'react-redux';
 
 function Total() {
@@ -18,7 +18,7 @@ function Total() {
 
   return (
       <div className="total">
-        <h2>ORDER SUMMARY</h2>
+        <h2>Order Summary</h2>
         <div>
           <p className="total__p">
             total ({getTotal().totalQuantity} items)
