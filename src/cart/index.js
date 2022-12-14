56fc48a2d69b0151249ccfb4a27be91908390b89
qinxhannah <qin.xia@northeastern.edu>
2022-12-13 15:54:14 -1000

    updated cart componemt with cart item section and total price section

--- src/cart/index.js
+++ src/cart/index.js
@@ -1,20 +1,58 @@
-import "./index.css"
-import {Link} from "react-router-dom";
+// import "./index.css"
+// import {Link} from "react-router-dom";
+//
+// const CartComponent = () => {
+//
+//
+//     return (
+//     <div>
+//       <h1>Your Cart</h1>
+//
+//         <div>Your cart is empty</div>
+//
+//         <Link to="/checkout">
+//         <button className={'checkout-button'} >Checkout</button>
+//         </Link>
+//     </div>
+//   );
+// };
+//
+// export default CartComponent;
 
-const CartComponent = () => {
+import './index.css'
+import cartItemArray from "./cartItems.json";
+import Total from './total';
+import CartItem from './cartItem';
+import { useSelector } from 'react-redux'
 
+function CartComponent() {
 
-    return (
-    <div>
-      <h1>Your Cart</h1>
+  const cart = useSelector((state) => state.cart)
 
-        <div>Your cart is empty</div>
+  return (
+      <div className="cart">
+        <div className="cart__left">
+          <div>
+            <h3>Shopping Cart</h3>
+            {cartItemArray.map((item) => (
+                <CartItem
+                    key={item.id}
+                    id={item.id}
+                    image={item.image}
+                    title={item.title}
+                    price={item.price}
+                    quantity={item.quantity}
+                />
+            ))}
+          </div>
+        </div>
 
-        <Link to="/checkout">
-        <button className={'checkout-button'} >Checkout</button>
-        </Link>
-    </div>
-  );
-};
+        <div className="cart__right">
+          <Total/>
+        </div>
+
+      </div>
+  )
+}
 
 export default CartComponent;
---
+++ src/cart/total.js
@@ -0,0 +1,32 @@
+import './total.css';
+import cartItemArray from "./cartItems.json";
+import {useSelector} from 'react-redux';
+
+function Total() {
+
+  const cart = useSelector((state) => state.cart)
+
+  const getTotal = () => {
+    let totalQuantity = 0
+    let totalPrice = 0
+    cartItemArray.forEach(item => {
+      totalQuantity += item.quantity
+      totalPrice += item.price * item.quantity
+    })
+    return {totalPrice, totalQuantity}
+  }
+
+  return (
+      <div className="total">
+        <h2>ORDER SUMMARY</h2>
+        <div>
+          <p className="total__p">
+            total ({getTotal().totalQuantity} items)
+            : <strong>${getTotal().totalPrice}</strong>
+          </p>
+        </div>
+      </div>
+  )
+}
+
+export default Total;
