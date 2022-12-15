9b1dd6b3eb4aa538bba374050324745aca822a85
Dennis Ping <idennisping@gmail.com>
2022-12-14 19:32:46 -0500

    Cleaned up some code

--- src/services/shop/shop-service.js
+++ src/services/shop/shop-service.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_API_BASE;
 
-export const getShop = async (contractAddress, page) => {
-  const response = await axios.get(`${API_BASE}/shop/${contractAddress}/${page}`);
+export const getShop = async (encodedName) => {
+  const response = await axios.get(`${API_BASE}/shop/${encodedName}`);
+  console.log(response.data);
   return response.data;
 };
--- src/shop/shop-grid.js
+++ src/shop/shop-grid.js
@@ -1,20 +1,8 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-
 import ShopItem from "./shop-item";
-import { getShopThunk } from "../services/shop/shop-thunk";
 
-const ShopGrid = ({ collection, page }) => {
-  const { shop, loading } = useSelector((state) => state.shop);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getShopThunk({ contractAddress: collection.contractAddress, page: page }));
-  }, [dispatch, collection.contractAddress, page]);
-
+const ShopGrid = ({ shop }) => {
   return (
     <>
-      {loading && <div>Loading...</div>}
-
       <div className="row gy-5">
         {shop.map((product) => (
           <ShopItem key={product.tokenId} product={product} />
